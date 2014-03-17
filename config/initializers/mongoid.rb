881f3313fbded788247a3c25348ab5d8b7cf5090
mtb24 <kendowney.com@gmail.com>
2014-03-16 23:10:02 -0700

    fix Mongo URI

--- config/initializers/mongoid.rb
+++ config/initializers/mongoid.rb
@@ -3,7 +3,7 @@ require 'uri'
 
 def get_connection
   return @db_connection if @db_connection
-  db = URI.parse(ENV['MONGOHQ_URL'])
+  db = URI.parse(ENV['MONGOLAB_URI'])
   db_name = db.path.gsub(/^\//, '')
   @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
   @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)

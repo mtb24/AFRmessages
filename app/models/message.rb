66d8b6c941d775ccc4a496e35d0a5bb84977fe70
mtb24 <kendowney.com@gmail.com>
2014-03-19 11:30:00 -0700

    comment out Message class

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message
-  include Mongoid::Document
-  field from type:String
-  field time type:Integer
-  field message type:String
+  # include Mongoid::Document
+  # field from type:String
+  # field time type:Integer
+  # field message type:String
 end

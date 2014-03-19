01eff1c5b5ca079839a6960651b60be4ffb93eb7
mtb24 <kendowney.com@gmail.com>
2014-03-19 12:03:46 -0700

    change mongoid config

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,26 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     #@users = User.all
+
+    require 'mongo'
+    require 'uri'
+
+    def get_connection
+      return @db_connection if @db_connection
+      db = URI.parse(ENV['MONGOHQ_URL'])
+      db_name = db.path.gsub(/^\//, '')
+      @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+      @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+      @db_connection
+    end
+
+    db = get_connection
+
+    puts "Collections"
+    puts "==========="
+    collections = db.collection_names
+    puts collections
+
   end
 =begin
 

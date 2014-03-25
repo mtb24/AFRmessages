c1972af54df0150574332fa92b16b3684c3d83a7
mtb24 <kendowney.com@gmail.com>
2014-03-24 21:36:45 -0700

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+	validates :phone_number, presence: true
+	validates :phone_number, numericality: { only_integer: true, :message => "Use digits only! No parens, spaces, dots, or dashes. or gears ;)"}
+	validates :phone_number, length: { is: 10 }
 end

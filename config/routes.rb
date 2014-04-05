b8862e05171a9a64793db66b1ad5d57774f4f095
mtb24 <kendowney.com@gmail.com>
2014-04-05 09:37:08 -0700

    Add users route

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Afrmessages::Application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   get 'broadcast' => 'messages#create'
+  get 'users' => 'users#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase

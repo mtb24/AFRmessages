1c3693ffd8aaa0aef2b6dda9fbb32cf860b42387
mtb24 <kendowney.com@gmail.com>
2016-04-06 21:44:35 -0700

    add basic auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate, :except => [:new, :show]
+  before_filter :authenticate, :except => [:new, :show, :create]
 
   # GET /users
   # GET /users.json

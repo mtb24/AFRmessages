25ff67d10d93d88c93c9be04778b9e860cfc66d6
mtb24 <kendowney.com@gmail.com>
2016-04-06 21:15:36 -0700

    add basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
+  helper_method :logged_in?
+
+  def logged_in?
+  	session[:login]
+  end
+
+  private
+  def authenticate
+  	login = authenticate_or_request_with_http_basic do |username, password|
+  		username == APP_CONFIG['username'] && Digest::SHA1.hexdigest(password) == APP_CONFIG['password']
+  	end
+  	session[:login] = login
+  end
+
+  def do_logout
+  	session[:login] = nil
+  end
 end

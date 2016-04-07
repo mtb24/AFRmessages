52854931f11aa8f3d8290102240444494a57c7a3
mtb24 <kendowney.com@gmail.com>
2016-04-06 21:28:59 -0700

    add resources directive back to routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Afrmessages::Application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   #resources :messages
-  #resources :users
+  resources :users
 
   # Example resource route with options:
   #   resources :products do
